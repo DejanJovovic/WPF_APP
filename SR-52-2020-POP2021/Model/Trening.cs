7301aa4648bc63fc31f7f47757e4537bb763a542
DejanJovovic <dejan.jovovic2002@gmail.com>
2021-12-06 21:07:25 +0100

    Trening klasa, modifikacija..

--- SR-52-2020-POP2021/Model/Trening.cs
+++ SR-52-2020-POP2021/Model/Trening.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SR_52_2020_POP2021.Model
 {
-    public class Trening
+    public class Trening : INotifyPropertyChanged
     {
         int id;
         DateTime datumTreninga;
@@ -43,14 +44,120 @@ namespace SR_52_2020_POP2021.Model
 
         }
 
-        public int Id { get => id; set => id = value; }
-        public DateTime DatumTreninga { get => datumTreninga; set => datumTreninga = value; }
-        public string VremePocetka { get => vremePocetka; set => vremePocetka = value; }
-        public int TrajanjeTreninga { get => trajanjeTreninga; set => trajanjeTreninga = value; }
-        public bool Slobodan { get => slobodan; set => slobodan = value; }
-        public Instruktor Instruktor { get => instruktor; set => instruktor = value; }
-        public Polaznik Polaznik { get => polaznik; set => polaznik = value; }
+        //public int Id { get => id; set => id = value; }
+        //public DateTime DatumTreninga { get => datumTreninga; set => datumTreninga = value; }
+        //public string VremePocetka { get => vremePocetka; set => vremePocetka = value; }
+        //public int TrajanjeTreninga { get => trajanjeTreninga; set => trajanjeTreninga = value; }
+        //public bool Slobodan { get => slobodan; set => slobodan = value; }
+        //public Instruktor Instruktor { get => instruktor; set => instruktor = value; }
+        //public Polaznik Polaznik { get => polaznik; set => polaznik = value; }
 
-       
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public DateTime DatumTreninga
+        {
+            get
+            {
+                return datumTreninga;
+            }
+            set
+            {
+                datumTreninga = value;
+                OnPropertyChanged("DatumTreninga");
+            }
+        }
+        public string VremePocetka
+        {
+            get
+            {
+                return vremePocetka;
+            }
+            set
+            {
+                vremePocetka = value;
+                OnPropertyChanged("VremePocetka");
+            }
+        }
+        public int TrajanjeTreninga
+        {
+            get
+            {
+                return trajanjeTreninga;
+            }
+            set
+            {
+                trajanjeTreninga = value;
+                OnPropertyChanged("TrajanjeTreninga");
+            }
+        }
+        public bool Slobodan
+        {
+            get
+            {
+                return slobodan;
+            }
+            set
+            {
+                slobodan = value;
+                OnPropertyChanged("Slobodan");
+            }
+        }
+
+        public Instruktor Instruktor
+        {
+            get
+            {
+                return instruktor;
+            }
+            set
+            {
+                instruktor = value;
+                OnPropertyChanged("Instruktor");
+            }
+        }
+        public Polaznik Polaznik
+        {
+            get
+            {
+                return polaznik;
+            }
+            set
+            {
+                polaznik = value;
+                OnPropertyChanged("Polaznik");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + DatumTreninga + " " + VremePocetka + " " + TrajanjeTreninga + " " + Slobodan + ", instruktor:" + Instruktor + ", polaznik:" + Polaznik;
+        }
+
+        // public Trening(int id, DateTime datumTreninga, string vremePocetka, int trajanjeTreninga, bool slobodan, Instruktor instruktor, Polaznik polaznik)
+
+
     }
 }
